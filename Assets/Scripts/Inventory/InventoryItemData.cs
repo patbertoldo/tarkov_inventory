5e29cbfaea62b6f8257a9597e9e341880e410914
Patrick Bertoldo <bertoldo.patrick@gmail.com>
2020-12-23 03:21:22 +1100

    Basic parameters and classes to create an inventory item.

---
+++ Assets/Scripts/Inventory/InventoryItemData.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Inventory
+{
+    [CreateAssetMenu(fileName = "Inventory Item Data File", menuName = "Custom/Inventory/Inventory Item Data File")]
+    [System.Serializable]
+    public class InventoryItemData : ScriptableObject
+    {
+        [SerializeField]
+        private ItemTypes itemType = 0;
+        public ItemTypes ItemType { get { return itemType; } }
+
+        [SerializeField]
+        private string itemName = "";
+        public string ItemName { get { return itemName; } }
+
+        [SerializeField]
+        private Sprite sprite = null;
+        public Sprite Sprite { get { return sprite; } }
+
+        [SerializeField]
+        private int sizeX = 1;
+        public int SizeX { get { return sizeX; } }
+
+        [SerializeField]
+        private int sizeY = 1;
+        public int SizeY { get { return sizeY; } }
+
+        public int Size { get { return SizeX * SizeY; } }
+    }
+}
---
+++ Assets/Scripts/ItemTypes.cs
@@ -0,0 +1,33 @@
+﻿public enum ItemTypes
+{
+    // If nothing applies:
+    Other,
+
+    // Weapons:
+    WeaponPrimary,
+    WeaponSecondary,
+    WeaponMelee,
+    WeaponPart,
+    Magazine,
+    Ammunition,
+    Throwable,
+
+    // Wearables:
+    EarPiece,
+    Headwear,
+    FaceCover,
+    ArmBand,
+    BodyArmor,
+    EyeWear,
+    TacticalVest,
+
+    // Consumables:
+    Medication,
+    Food,
+    Drink,
+
+    Currency,
+    Container,
+    Key,
+    Special,
+}
